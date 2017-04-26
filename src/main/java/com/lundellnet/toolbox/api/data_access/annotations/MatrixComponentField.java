b207edc2d798b41aac5b0f6f8caf7c577dd48856
Nate Lundell <nate@lundellnet.com>
2017-04-26 13:59:38 -0500

    Added MatrixCompoenentField annotation, changed the valueMapping to return an array of PointMapping in MatricComponentPoint, and updated AnnotatedElementType enum

--- src/main/java/com/lundellnet/toolbox/api/data_access/annotations/MatrixComponentField.java
+++ src/main/java/com/lundellnet/toolbox/api/data_access/annotations/MatrixComponentField.java
@@ -23,9 +23,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD})
-public @interface MatrixSource {
-    Class<?> sourceType();
-    
-    MappingProperty[] properties() default {};
+@Target({ElementType.FIELD, ElementType.TYPE})
+public @interface MatrixComponentField {
+
 }
--- src/main/java/com/lundellnet/toolbox/api/data_access/annotations/MatrixComponentPoint.java
+++ src/main/java/com/lundellnet/toolbox/api/data_access/annotations/MatrixComponentPoint.java
@@ -25,5 +25,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.TYPE})
 public @interface MatrixComponentPoint {
-    PointMapping valueMapping();
+    PointMapping[] valueMapping();
 }
